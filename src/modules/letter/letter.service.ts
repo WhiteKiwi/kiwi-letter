7818a9180711371fac4715e5234e916c29175586
kiwi-jang <kiwi.jang@toss.im>
2022-12-10 03:26:18 +0900

    ymd 및 escape 슥슥

--- src/modules/letter/letter.service.ts
+++ src/modules/letter/letter.service.ts
@@ -23,9 +23,17 @@ export class LetterService {
 	}): Promise<void> {
 		await this.letterModel.create({
 			name,
-			content,
-			createdAt: '',
+			content: content.replace(/</g, '&lt;'),
+			createdAt: dateToYyyyMMdd(new Date()),
 			sended: false,
 		});
 	}
+}
+
+function dateToYyyyMMdd(date: Date) {
+	const year = date.getFullYear();
+	const month = ('0' + (1 + date.getMonth())).slice(-2);
+	const day = ('0' + date.getDate()).slice(-2);
+
+	return [year, month, day].join('-');
 }
